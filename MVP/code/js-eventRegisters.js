4b17458bc21b3a9fdc8dde436ff294b82e907372
Xia Hua <huaxiairving@gmail.com>
2020-04-10 13:37:56 -0600

    1

--- MVP/code/js-eventRegisters.js
+++ MVP/code/js-eventRegisters.js
@@ -1,3 +1,28 @@
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+//btn.onclick = function() {
+//  modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
 document.getElementById("key-01").addEventListener("click", pushKeybuttons, true );
 document.getElementById("key-02").addEventListener("click", pushKeybuttons, true );
 document.getElementById("key-03").addEventListener("click", pushKeybuttons, true );
@@ -10,3 +35,4 @@ document.getElementById("key-09").addEventListener("click", pushKeybuttons, true
 document.getElementById("key-00").addEventListener("click", pushKeybuttons, true );
 document.getElementById("key-send").addEventListener("click", pushKeybuttons, true );
 document.getElementById("key-delete").addEventListener("click", pushKeybuttons, true );
+
